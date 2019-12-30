41f568768a9f43b97563ce613503e3fbb788a8ff
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 11:15:54 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1577684778,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/seeanswer-sheet0.png",
+		"images/playbackground2000-sheet0.png",
+		"images/quizbackground-sheet0.png",
+		"images/quiz-sheet0.png",
+		"images/hudbest-sheet0.png",
+		"images/hudscore-sheet0.png",
+		"images/hudlevel-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"quiz_en.txt",
+		"quiz_ko.txt"
+	]
+}
